5011a080f5891e5b362b33c0a3029d8b359aac8e
ExillustX <mrexillustx@gmail.com>
2019-06-12 09:48:42 +0100

    add code

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log('disboardBump bot started...');
+  bump();
+  setInterval(function() {
+    bump();
+  }, process.env.INTERVAL);
+});
+
+function bump() {
+    client.channels.get(process.env.CHANNEL).send('!disboard bump');
+};
+
+client.login(process.env.TOKEN);
